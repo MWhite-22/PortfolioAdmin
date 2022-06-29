406a75c30247808ea47034ac7fbdb15064da9abd
Michael White <michael@mwhite.io>
2022-06-29 18:11:11 -0400

    Add some basic styling and playing with tailwind

--- src/layouts/rootLayout.tsx
+++ src/layouts/rootLayout.tsx
@@ -1,4 +1,6 @@
+import { SunIcon, MoonIcon } from '@heroicons/react/outline';
 import Head from 'next/head';
+import { useState } from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 interface Props {
@@ -6,6 +8,8 @@ interface Props {
 }
 
 export const RootLayout: React.FC<Props> = ({ children }) => {
+	const [darkMode, setDarkMode] = useState(false);
+
 	return (
 		<>
 			<Head>
@@ -13,14 +17,28 @@ export const RootLayout: React.FC<Props> = ({ children }) => {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<div className='flex min-h-screen flex-col'>
-				<header className='flex border-b-2 p-4'>
+				<header className='flex items-center justify-between border-b-2 p-4 print:hidden'>
 					<h1>Root Layout Header</h1>
+					<button
+						className='flex h-8 w-8 items-center justify-center rounded-full bg-slate-400 hover:cursor-pointer hover:bg-slate-500 dark:bg-yellow-300 dark:text-slate-800 dark:hover:bg-yellow-100'
+						onClick={() => {
+							document.documentElement.classList.toggle('dark');
+							setDarkMode((prev) => !prev);
+						}}
+					>
+						{darkMode ? <SunIcon className='p-1' /> : <MoonIcon className='p-1' />}
+					</button>
 				</header>
 
 				<main className='flex-grow p-4'>{children}</main>
 
-				<footer className='border-t-2 p-4 text-center'>
-					<h4>Root Layout Footer</h4>
+				<footer className='flex  flex-col border-t-2 bg-slate-200 p-4 text-center dark:bg-slate-700'>
+					<div className='mb-2 flex-grow print:hidden'>
+						<h5>OTHER CONTENT TO GO DOWN HERE...</h5>
+					</div>
+					<p className='text-center text-xs uppercase'>
+						Copyright &copy; Michael J White 2020 - {new Date().getFullYear()}{' '}
+					</p>
 				</footer>
 			</div>
 
